e1ac5322a8f77096b6509d1233204961fe122d39
6811130001phisitMUT <6811130001@mut.ac.th>
2025-11-25 17:12:46 +0700

    123

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,9 @@
+// TEST01.cpp
+#include <iostream>
+
+int main() {
+    std::cout << "Name: phisit lophiriyakun" << std::endl;
+    std::cout << "Course: MIIA0106" << std::endl;
+    std::cout << "Email:6811130001@mut.ac.th" << std::endl;
+    return 0;
+}
